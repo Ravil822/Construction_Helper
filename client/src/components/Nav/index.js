b64c8d8547e3c7d6817179e907ba84faac90e060
RAvil <russian82@mail.com>
2020-04-24 18:31:19 -0400

    fixed nav bar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,93 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import { useStoreContext } from "../../utils/GlobalState";
 import "./style.css";
 import { Link, useLocation, withRouter } from "react-router-dom";
 import auth0Client from '../../Auth';
 
+
 function Nav(props) {
+   const [menu, setMenu] = useState(false)
+   const showMenu = () => {
+      var listItem = document.getElementsByClassName("item");
+      Array.prototype.forEach.call(listItem, element => {
+         element.style.display = !menu ? "block" : "none"
+      });
+      setMenu(!menu)
+   }
 
    const signOut = () => {
       auth0Client.signOut();
       props.history.replace('/');
-    };
-  const [store] = useStoreContext();
-  const location = useLocation();
+   };
+   const [store] = useStoreContext();
+   const location = useLocation();
 
-  return (
-    <>
-    <div id="top" class="top-header hidden-sm hidden-xs">
-         <div class="container">
-            <div class="row">
-               <div class="col-xs-12 col-md-6">
-                  <div class="top-number">
-                     <p>
-                        <i class="fa fa-phone"></i>  +1 900 234 567, +1 900 234 568
+   return (
+      <div>
+         <div id="top" class="top-header hidden-sm hidden-xs">
+            <div class="container">
+               <div class="row">
+                  <div class="col-xs-12 col-md-6">
+                     <div class="top-number">
+                        <p>
+                           <i class="fa fa-phone"></i>  +1 900 234 567, +1 900 234 568
                         &nbsp;&nbsp;&nbsp; <i class="fa fa-envelope-o"></i> Mail : info@osahan.com
                      </p>
+                     </div>
                   </div>
-               </div>
-               <div class="col-xs-12 col-md-6 text-right">
-                  <ul class="social-share-top">
-                     <a href="#" class="btn-social btn-behance"><i class="fa fa-behance"></i></a>
-                     <a href="#" class="btn-social btn-dribbble"><i class="fa fa-dribbble"></i></a>
-                     <a href="#" class="btn-social btn-facebook"><i class="fa fa-facebook"></i></a>
-                     <a href="#" class="btn-social btn-google-plus"><i class="fa fa-google-plus"></i></a>
-                     <a href="#" class="btn-social btn-instagram"><i class="fa fa-instagram"></i></a>
-                     <a href="#" class="btn-social btn-linkedin"><i class="fa fa-linkedin"></i></a>
-                     <a href="#" class="btn-social btn-pinterest"><i class="fa fa-pinterest"></i></a>
-                     <a href="#" class="btn-social btn-skype"><i class="fa fa-skype"></i></a>
-                     <a href="#" class="btn-social btn-twitter"><i class="fa fa-twitter"></i></a>
-                     <a href="#" class="btn-social btn-youtube"><i class="fa fa-youtube"></i></a>
-                     <a href="#" class="btn-social btn-email"><i class="fa fa-envelope-o"></i></a>
-                  </ul>
+                  <div class="col-xs-12 col-md-6 text-right">
+                     <ul class="social-share-top">
+                        <a href="#" class="btn-social btn-behance"><i class="fa fa-behance"></i></a>
+                        <a href="#" class="btn-social btn-dribbble"><i class="fa fa-dribbble"></i></a>
+                        <a href="#" class="btn-social btn-facebook"><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="btn-social btn-google-plus"><i class="fa fa-google-plus"></i></a>
+                        <a href="#" class="btn-social btn-instagram"><i class="fa fa-instagram"></i></a>
+                        <a href="#" class="btn-social btn-linkedin"><i class="fa fa-linkedin"></i></a>
+                        <a href="#" class="btn-social btn-pinterest"><i class="fa fa-pinterest"></i></a>
+                        <a href="#" class="btn-social btn-skype"><i class="fa fa-skype"></i></a>
+                        <a href="#" class="btn-social btn-twitter"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="btn-social btn-youtube"><i class="fa fa-youtube"></i></a>
+                        <a href="#" class="btn-social btn-email"><i class="fa fa-envelope-o"></i></a>
+                     </ul>
+                  </div>
                </div>
             </div>
          </div>
-      </div>
-    <nav>
-      {/* <ul class="Mass"><li> Constuction Helper</li></ul> */}
-<ul class="menu">
-         <li class="logo" style = {{color: 'white'}}>
-        You Nailed It! Construction
+         <nav>
+            {/* <ul class="Mass"><li> Constuction Helper</li></ul> */}
+            <ul class="menu">
+               <li class="logo" style={{ color: 'white' }}>
+                  You Nailed It! Construction
         </li>
-        <li class="item"> <Link to="/" className={location.pathname === "/" ? "activate" : "notactivate"}>Home </Link></li>
-        <li class="item">
-        <Link to="/projects" className={location.pathname === "/projects" ? "activate" : "notactivate"}>Projects </Link> </li>
-        
-        
-        <li class="item">
-        <Link to="/contact" className={location.pathname === "/contact" ? "activate" : "notactivate"}>Contact </Link></li>
-        {
-        auth0Client.isAuthenticated() &&
-        <li class="item">
-        <Link to="/setwork" className={location.pathname === "/setwork" ? "activate" : "notactivate"}>Display </Link></li>
-         }
-         {
-         auth0Client.isAuthenticated() &&
-         <li class="item">
-         <Link to="/startProject" className={location.pathname === "/startProject" ? "activate" : "notactivate"}>Add Project </Link></li>
-          }
-        
-        {
-        !auth0Client.isAuthenticated() &&
-        <button className="btn btn-dark item button" onClick={auth0Client.signIn}>Sign In or Sign Up</button>
-      }
-        
-        {/* <li class="item button"><a href="#">Log In</a></li>
+               <li class="item"> <Link to="/" className={location.pathname === "/" ? "activate" : "notactivate"}>Home </Link></li>
+               <li class="item">
+                  <Link to="/projects" className={location.pathname === "/projects" ? "activate" : "notactivate"}>Projects </Link> </li>
+
+
+               <li class="item">
+                  <Link to="/contact" className={location.pathname === "/contact" ? "activate" : "notactivate"}>Contact </Link></li>
+               {
+                  auth0Client.isAuthenticated() &&
+                  <li class="item">
+                     <Link to="/setwork" className={location.pathname === "/setwork" ? "activate" : "notactivate"}>Display </Link></li>
+               }
+               {
+                  auth0Client.isAuthenticated() &&
+                  <li class="item">
+                     <Link to="/startProject" className={location.pathname === "/startProject" ? "activate" : "notactivate"}>Add Project </Link></li>
+               }
+
+               {
+                  !auth0Client.isAuthenticated() &&
+                  <button className="btn btn-dark item button" onClick={auth0Client.signIn}>Sign In or Sign Up</button>
+               }
+
+               {/* <li class="item button"><a href="#">Log In</a></li>
         <li class="item button secondary"><a href="#">Sign Up</a></li> */}
-        {
-        auth0Client.isAuthenticated() &&
-        <li class="item button secondary">
-          <label className="mr-2 text-white">{auth0Client.getProfile().name}</label>
-          <button className="btn btn-dark" onClick={() => {signOut()}}>Sign Out</button>
-        </li>
-      }
-        <li class="toggle"><a href="#"><i class="fas fa-bars"></i></a></li>
-        </ul> 
-        
-</nav>
-</>
-  );
+               {
+                  auth0Client.isAuthenticated() &&
+                  <li class="item button secondary">
+                     <label className="mr-2 text-white">{auth0Client.getProfile().name}</label>
+                     <button className="btn btn-dark" onClick={() => { signOut() }}>Sign Out</button>
+                  </li>
+               }
+               <li class="toggle"><a href="#" onClick={showMenu}><i class="fas fa-bars"></i></a></li>
+            </ul>
+
+         </nav>
+      </div>
+   );
 }
 
 export default withRouter(Nav);
